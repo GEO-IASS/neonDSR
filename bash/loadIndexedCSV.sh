5d89036b9af806f6e8b816bd11392e3d35228539
mshahriarinia <info@mshahriarinia.com>
2013-10-17 11:32:06 -0400

    loadIndexedCSV takes csv file name and desired array name as parameters.

--- bash/loadIndexedCSV.sh
+++ bash/loadIndexedCSV.sh
@@ -1,34 +1,53 @@
 #!/bin/bash
 
-DATA_PATH=/home/scidb/zproject/neonDSR/matlab
+#DATA_PATH=/home/scidb/zproject/neonDSR/matlab
 
-echo "Convert hsi_img.csv to .scidb"
-csv2scidb -s 1 -p NNNN < $DATA_PATH"/hsi_img.csv" > $DATA_PATH"/hsi_img.scidb"
+echo "Convert .csv to .scidb"
+echo "----------------------------------------------------------------"
+
+if [ -z "$1" ] 
+then
+  echo "Error calling script."
+  echo "Usage: \$ scirpt file_name scdb_array_name"
+fi
+
+if [ -z "$2" ] 
+then
+  echo "Error calling script."
+  echo "Usage: \$ scirpt scdb_array_name"
+fi
+
+DATA_PATH=$(dirname $1)
+FILE_NAME=$(basename $1)
+echo "$1 to .scidb"
+time csv2scidb -s 1 -p NNNN < $1 > $DATA_PATH"/"$FILE_NAME".scidb"
 
 
-echo "Remove hsi_img_flat array from scidb"
-iquery -aq "remove(hsi_img_flat);"
+echo "Remove $2_flat array from scidb"
+iquery -aq "remove($2_flat);"
 
-echo "Create hsi_img_img array"
-iquery -aq "create array hsi_img_flat <x:int32, y:int32, wave_length:int32, val:int32> [i=0:*,1000000,0];"
+echo "Create $2_flat array"
+time iquery -aq "create array $2_flat <x:int32, y:int32, wave_length:int32, val:int32> [i=0:*,1000000,0];"
 
-echo "Load hsi_img_flat.scidb to hsi_img_flat array"
-iquery -anq "load(hsi_img_flat, '$DATA_PATH/hsi_img.scidb');"
+echo "Load $DATA_PATH/$FILE_NAME.scidb to $2_flat array"
+time iquery -anq "load($2_flat, '$DATA_PATH/$FILE_NAME.scidb');"
 
-echo "Remove hsi_img array from scidb if exists"
-iquery -aq "remove(hsi_img);"
+echo "Remove $2 array from scidb if exists"
+iquery -aq "remove($2);"
 
-X_MAX=$( cat $DATA_PATH"/hsi_img.csv" | cut -f1 -d "," | sort -nr | head -1)
-Y_MAX=$( cat $DATA_PATH"/hsi_img.csv" | cut -f2 -d "," | sort -nr | head -1)
-W_MAX=$( cat $DATA_PATH"/hsi_img.csv" | cut -f3 -d "," | sort -nr | head -1)
-W_MIN=$(tail -n +2  $DATA_PATH"/hsi_img.csv"  | cut -f3 -d "," | sort -n  | head -1)
+X_MAX=$( cat $1 | cut -f1 -d "," | sort -nr | head -1)
+Y_MAX=$( cat $1 | cut -f2 -d "," | sort -nr | head -1)
+W_MAX=$( cat $1 | cut -f3 -d "," | sort -nr | head -1)
+W_MIN=$(tail -n +2  $1  | cut -f3 -d "," | sort -n  | head -1)
 
 CHUNK_SIZE=1000
 
-echo "Create hsi_img array"
-echo "create array hsi_img <val:int32> [x=0:$X_MAX,$CHUNK_SIZE,0, y=0:$Y_MAX,$CHUNK_SIZE,0, w=0:$W_MAX,$CHUNK_SIZE,0];"
+echo "Create $2 array"
+echo "create array $2 <val:int32> [x=0:$X_MAX,$CHUNK_SIZE,0, y=0:$Y_MAX,$CHUNK_SIZE,0, w=0:$W_MAX,$CHUNK_SIZE,0];"
 
-iquery -aq "create array hsi_img <val:int32> [x=0:$X_MAX,$CHUNK_SIZE,0, y=0:$Y_MAX,$CHUNK_SIZE,0, w=$W_MIN:$W_MAX,$CHUNK_SIZE,0];"
+time iquery -aq "create array $2 <val:int32> [x=0:$X_MAX,$CHUNK_SIZE,0, y=0:$Y_MAX,$CHUNK_SIZE,0, w=$W_MIN:$W_MAX,$CHUNK_SIZE,0];"
 
-iquery -aq "redimension_store(hsi_img_flat, hsi_img)";
+time iquery -aq "redimension_store($2_flat, $2)";
 
+# Clean-up
+iquery -aq "remove($2_flat);
