70545ad1569e5b2de0c56ce6fe8b9b257be0d49f
mshahriarinia <info@mshahriarinia.com>
2014-01-15 11:45:29 -0500

    Refactor

--- code/matlab/main.m
+++ code/matlab/main.m
@@ -119,29 +119,6 @@ for i=1:spice_params.M
     saveas(h,sprintf('endmember-%1d', i),'png');
 end
 
-
-
-%% PCOMMEND: non-linear unmixing, well suited for neon dataset
-
-addpath('/home/scidb/zproject/neonDSR/matlab/uf/PCOMMEND');
-
-%[n_row,n_col,n_band] = size(subimg); 
-
-%subimg_data = reshape(subimg,n_row*n_col,n_band)';
-
-params = PCOMMEND_Parameters();
-E = PCOMMEND(double(sub_data), params);
-
-for b = 1:length(E)
-  figure(b)
-  T = E{b};
-  plot(T(1, :)); ylim([0 1]);
-  hold on
-  plot(T(2, :)); ylim([0 1]);
-  hold off
-end
-
-
 %% SPICEE: The correct version of Spice by Taylor
 
 addpath('/home/scidb/zproject/neonDSR/code/matlab/uf/spicee');
@@ -175,3 +152,69 @@ for i=1:size(P,2)   % only show non-pruned endmembers
 end
 
 close all;
+
+%% PCOMMEND: non-linear unmixing, well suited for neon dataset
+
+addpath('/home/scidb/zproject/neonDSR/matlab/uf/PCOMMEND');
+
+%[n_row,n_col,n_band] = size(subimg); 
+
+%subimg_data = reshape(subimg,n_row*n_col,n_band)';
+
+params = PCOMMEND_Parameters();
+E = PCOMMEND(double(sub_data), params);
+
+for b = 1:length(E)
+  figure(b)
+  T = E{b};
+  plot(T(1, :)); ylim([0 1]);
+  hold on
+  plot(T(2, :)); ylim([0 1]);
+  hold off
+end
+
+%% PCOMMEND: Taylor snippet
+
+%campus1_setup; % replace this with the setup for your data
+
+addpath('/home/scidb/zproject/neonDSR/matlab/uf/PCOMMEND');
+
+envi = enviread('/home/scidb/neon/f100910t01p00r02rdn/f100910t01p00r02rdn_b_NEON-L1G/f100910t01p00r02rdn_b_sc01_ort_flaashreflectance_img');
+hsi_img = envi.z;
+
+subimg = hsi_img(1200:1400 , 400:600, :);
+
+params = PCOMMEND_Parameters;
+params.iterationCap = 100;
+params.C = 4;
+params.M = 3;
+
+[n_row,n_col,n_band] = size(subimg);
+n_pix = n_row*n_col;
+
+hsi_data = double(reshape(subimg,n_row*n_col,n_band));
+
+[E,P,U,obj] = PCOMMEND(hsi_data',params); %wants data to be n_pix x n_band
+
+for i=1:params.C
+    % show membership map for each partition
+    figure(10+i);
+    mem = U(i,:);
+    imagesc(reshape(mem,[n_row,n_col]),[0 1]);
+    colorbar;
+    title(sprintf('PCOMMEND region %d membership',i));
+    
+    % plot endmembers for each partition
+    figure(20+i);
+    plot(E{i}');    
+    title(sprintf('PCOMMEND region %d endmembers',i));
+
+    % plot proportion maps for each endmember
+    figure(30+i);
+    for j=1:params.M
+        subplot(2,2,j);
+        imagesc(reshape(P{i}(:,j),[n_row,n_col]),[0 1]);
+        title(sprintf('proportion map %d,%d',i,j));    
+    end
+end
+
