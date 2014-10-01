57b40253d0c76a016c16827bb60766f272b5d1dc
mshahriarinia <info@mshahriarinia.com>
2014-09-30 21:11:54 -0400

    Backup

--- code/matlab/io/get_field_pixels.m
+++ code/matlab/io/get_field_pixels.m
@@ -1,9 +1,14 @@
 function [ specie, reflectance ] = get_field_pixels(  )
-
+% load field data csv file and return items.
 global setting;
 
-field_trips = csvread(setting.FIELD_PATH);
+field_trips = csv_read(setting.FIELD_PATH);
+specie = field_trips(:,5);
+specie(1,:) = [];
+reflectance = field_trips(:,13:236);
+reflectance(1,:) = [];
 
+roi = field_trips(:,3);
 
 end
 
