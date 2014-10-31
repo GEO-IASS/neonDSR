425115732d550b40d5dec849ce98d0a55451d0bf
Morteza Shahriari Nia <m.shahriarinia@gmail.com>
2014-10-31 18:26:14 -0400

    try the all the pixels again, hopefully get better results than just using high ndvi pixels

--- code/matlab/svm/matlab_svm/main_svm_canopy_based.m
+++ code/matlab/svm/matlab_svm/main_svm_canopy_based.m
@@ -116,26 +116,131 @@ title(sprintf('Effects of Gaussian Window Size on Classification Accuracy \n (ca
 figure;
 %semilogx(polynomial_orders(1:numel(polynomial_orders)), svm_results_poly(1:numel(polynomial_orders)),'marker', 's');
 %plot(setting.SVM_POLYNOMIAL_ORDERS, svm_poly_atcor);
-plot(libsvm_poly_atcor)
+plot(svm_poly_atcor)
 figure 
 surf(setting.LIBSVM_COST_VALUES, setting.SVM_POLYNOMIAL_ORDERS, svm_poly_atcor)
 zlabel('Prediction Accuracy')
 xlabel('C'); ylabel('Polynomial Degree');
-[v,ind]=max(libsvm_poly_atcor);
-[v1,ind1]=max(max(libsvm_poly_atcor));
+[v,ind]=max(svm_poly_atcor);
+[v1,ind1]=max(max(svm_poly_atcor));
 disp(sprintf('The largest element in this matrix is %f at (%d,%d).', v1, ind(ind1), ind1 ));
 title(sprintf('Effects of Polynomial Order on Classification Accuracy (canopy-based)'));
 
 figure;
 %semilogx(setting.SVM_RBF_SIGMA_VALUES, svm_rbf_atcor);
 %grid on
-plot(libsvm_rbf_atcor)
+plot(svm_rbf_atcor)
 figure 
 surf(setting.LIBSVM_COST_VALUES, setting.SVM_RBF_SIGMA_VALUES, svm_rbf_atcor)
 zlabel('Prediction Accuracy')
 xlabel('C'); ylabel('Polynomial Degree');
-[v,ind]=max(libsvm_rbf_atcor);
-[v1,ind1]=max(max(libsvm_rbf_atcor));
+[v,ind]=max(svm_rbf_atcor);
+[v1,ind1]=max(max(svm_rbf_atcor));
+disp(sprintf('The largest element in this matrix is %f at (%d,%d).', v1, ind(ind1), ind1 ));
+xlabel('SVM Kernel - RBF (\sigma)'); ylabel('Accuracy (%)');
+title('Effects of RBF Kernel \sigma on SVM Classification Accuracy (canopy-based)');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+%% ON ALL PIXELS NOT JUST REMOVED NDVIS
+
+
+
+tic
+[libsvm_gaussian_atcorFULL, libsvm_poly_atcorFULL, libsvm_rbf_atcorFULL] = get_svm_statistics_canopy_based(species, reflectances, rois, 'libsvm');
+toc
+
+figure;
+plot(setting.SVM_GAUSSIAN_SMOOTHING_WINDOWS, libsvm_gaussian_atcorFULL);
+xlabel('Gaussian window size'); ylabel('Accuracy (%)');
+title(sprintf('Effects of Gaussian Window Size on Classification Accuracy \n (canopy-based) - Polynimial Kernel Order 3'));
+
+figure;
+%semilogx(polynomial_orders(1:numel(polynomial_orders)), svm_results_poly(1:numel(polynomial_orders)),'marker', 's');
+%plot(setting.SVM_POLYNOMIAL_ORDERS, svm_poly_atcor);
+plot(libsvm_poly_atcorFULL)
+figure 
+surf(setting.LIBSVM_COST_VALUES, setting.SVM_POLYNOMIAL_ORDERS, libsvm_poly_atcorFULL)
+zlabel('Prediction Accuracy')
+xlabel('C'); ylabel('Polynomial Degree');
+[v,ind]=max(libsvm_poly_atcorFULL);
+[v1,ind1]=max(max(libsvm_poly_atcorFULL));
+disp(sprintf('The largest element in this matrix is %f at (%d,%d).', v1, ind(ind1), ind1 ));
+title(sprintf('Effects of Polynomial Order on Classification Accuracy (canopy-based)'));
+
+figure;
+%semilogx(setting.SVM_RBF_SIGMA_VALUES, svm_rbf_atcor);
+%grid on
+plot(libsvm_rbf_atcorFULL)
+figure 
+surf(setting.LIBSVM_COST_VALUES, setting.SVM_RBF_SIGMA_VALUES, libsvm_rbf_atcorFULL)
+zlabel('Prediction Accuracy')
+xlabel('C'); ylabel('Polynomial Degree');
+[v,ind]=max(libsvm_rbf_atcorFULL);
+[v1,ind1]=max(max(libsvm_rbf_atcorFULL));
+disp(sprintf('The largest element in this matrix is %f at (%d,%d).', v1, ind(ind1), ind1 ));
+xlabel('SVM Kernel - RBF (\sigma)'); ylabel('Accuracy (%)');
+title('Effects of RBF Kernel \sigma on SVM Classification Accuracy (canopy-based)');
+
+
+
+
+
+
+
+
+
+tic
+[svm_gaussian_atcorFULL, svm_poly_atcorFULL, svm_rbf_atcorFULL] = get_svm_statistics_canopy_based(species, reflectances, rois, 'matlab');
+toc
+
+figure;
+plot(setting.SVM_GAUSSIAN_SMOOTHING_WINDOWS, svm_gaussian_atcorFULL);
+xlabel('Gaussian window size'); ylabel('Accuracy (%)');
+title(sprintf('Effects of Gaussian Window Size on Classification Accuracy \n (canopy-based) - Polynimial Kernel Order 3'));
+
+figure;
+%semilogx(polynomial_orders(1:numel(polynomial_orders)), svm_results_poly(1:numel(polynomial_orders)),'marker', 's');
+%plot(setting.SVM_POLYNOMIAL_ORDERS, svm_poly_atcor);
+plot(svm_poly_atcorFULL)
+figure 
+surf(setting.LIBSVM_COST_VALUES, setting.SVM_POLYNOMIAL_ORDERS, svm_poly_atcorFULL)
+zlabel('Prediction Accuracy')
+xlabel('C'); ylabel('Polynomial Degree');
+[v,ind]=max(svm_poly_atcorFULL);
+[v1,ind1]=max(max(svm_poly_atcorFULL));
+disp(sprintf('The largest element in this matrix is %f at (%d,%d).', v1, ind(ind1), ind1 ));
+title(sprintf('Effects of Polynomial Order on Classification Accuracy (canopy-based)'));
+
+figure;
+%semilogx(setting.SVM_RBF_SIGMA_VALUES, svm_rbf_atcor);
+%grid on
+plot(svm_rbf_atcorFULL)
+figure 
+surf(setting.LIBSVM_COST_VALUES, setting.SVM_RBF_SIGMA_VALUES, svm_rbf_atcorFULL)
+zlabel('Prediction Accuracy')
+xlabel('C'); ylabel('Polynomial Degree');
+[v,ind]=max(svm_rbf_atcorFULL);
+[v1,ind1]=max(max(svm_rbf_atcorFULL));
 disp(sprintf('The largest element in this matrix is %f at (%d,%d).', v1, ind(ind1), ind1 ));
 xlabel('SVM Kernel - RBF (\sigma)'); ylabel('Accuracy (%)');
 title('Effects of RBF Kernel \sigma on SVM Classification Accuracy (canopy-based)');
@@ -153,3 +258,5 @@ title('Effects of RBF Kernel \sigma on SVM Classification Accuracy (canopy-based
 % the final result of cross-validation would be trained on all the data
 % using the parameters that had the best accuracy in average of its k-fold
 % runs.
+
+
