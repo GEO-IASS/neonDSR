bdafb532419c6f4e19a13fdbfe7e76f6c08d28be
Morteza Shahriari Nia <m.shahriarinia@gmail.com>
2015-04-10 15:54:16 -0400

    removed clutter

--- code/matlab/svm/main_svm_performance.m
+++ code/matlab/svm/main_svm_performance.m
@@ -15,9 +15,12 @@ for i=1: size(reflectances, 1)
     reflectances(i,:) = scalePixel(reflectances(i,:));
 end
 
-figure, plot(setting.wavelength, reflectances'); title('Field Data - Original Form');
+figure, subplot(1,2,1);
+plot(setting.wavelength, reflectances'); title('Field Data - Original Form');
 %set(gca,'XTick', 400:200:2500);
+axis([350 2500 0 1])
 set(gca,'XTick', 500:500:2500);
+set(gca,'fontsize',12)
 xlabel('Wavelength (nm)'), ylabel('Reflectance');
 
 % reflectances_rwab1 = removeWaterAbsorbtionBands(reflectances,1);
@@ -33,11 +36,15 @@ xlabel('Wavelength (nm)'), ylabel('Reflectance');
 % xlabel('Wavelength (nm)'), ylabel('Reflectance');
 
 reflectances_g16 = gaussianSmoothing(reflectances, 8);
-figure, plot(setting.wavelength, reflectances_g16'); title('Field Data - Gaussian Smoothing 8');
-pp = 24;
+%figure, 
+subplot(1,2,2);
+plot(setting.wavelength, reflectances_g16'); title('Field Data - Gaussian Smoothing 8');
+%pp = 24;
 %set(gcf,'PaperUnits','points','PaperSize',[pp,pp],'PaperPosition',[0,0,pp,pp])
 %set(gcf,'PaperUnits','inches','PaperPosition',[0 0 4*.8 3*.8])
+axis([350 2500 0 1])
 set(gca,'XTick', 500:500:2500);
+set(gca,'fontsize',12)
 xlabel('Wavelength (nm)'), ylabel('Reflectance');
 %print -dpng filename.png -r100
 
